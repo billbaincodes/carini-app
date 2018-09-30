98ab6a27e9247889e253df75d74659479dacaa37
bill <williamandrewbain@gmail.com>
2018-09-30 13:36:16 -0600

    set getter displays songs from random set

--- src/SetGetter.js
+++ src/SetGetter.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let regEx = /(?<='>)(.*?)(?=<)/g
+let regEx = /(?<=g'>)(.*?)(?=<)/gmi
 
 class SetGetter extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      sortedList: [],
+    }
+  }
 
 
   setFetch = () => {
     fetch('https://api.phish.net/v3/setlist/random?apikey=5B8686EDCD6647974F51')
     .then(response => response.json())
     .then(randomShow => {
-      let setlist = randomShow.response.data[0].setlistdata
-      setlist = setlist.toString()
-
-      console.log(regEx.exec(setlist))
+      let setlist = randomShow.response.data[0].setlistdata.toString()
+      let sortedList = setlist.match(regEx)
+      this.setState({sortedList: sortedList})
       }
     )
   }
@@ -23,6 +29,7 @@ class SetGetter extends Component {
       <div>
         <h3>come get ya sets</h3>
         <button onClick={this.setFetch}>Sets Here!</button>
+        <div>{this.state.sortedList}</div>
       </div>
 
     );

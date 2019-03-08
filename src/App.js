576c2ddaa0ffb2686e45f6bf66c1f4b774b04c20
bill <williamandrewbain@gmail.com>
2019-03-07 21:59:41 -0700

    Add splash page content

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ class App extends Component {
       <div className="container">
         <h2>Makisupa SetTool</h2>
         <h4>Setlists come to your house, use the buttons to obtain a random or dated set list. CORS required.</h4>
+        <li>
+          <ul>
+            <li>Lyia Tiefling</li>
+            <li>Neero Triton</li>
+            <li>Amanda</li>
+            <li>Tweezer -> Death Don't Hurt Very Long ->  Tweezer</li>
+          </ul>
+        </li>
         <div className="buttonContainer">
           <RandomSetGetter /> 
           <GetSetByDate />
